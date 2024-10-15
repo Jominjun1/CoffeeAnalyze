81817842e5a7ce44b1692cc98547d26b006ca645
Jominjun1 <simpers@naver.com>
2024-10-15 14:22:19 +0900

    관리자 기능 틀 제작

--- src/main/java/com/example/coffeeproject/controller/AdminController.java
+++ src/main/java/com/example/coffeeproject/controller/AdminController.java
@@ -4,9 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/user")
+@RequestMapping("/admin")
 @RestController
 @Controller
-public class UserController {
-
+public class AdminController {
 }
---
+++ src/main/java/com/example/coffeeproject/service/AdminService.java
@@ -0,0 +1,38 @@
+package com.example.coffeeproject.service;
+
+import com.example.coffeeproject.model.Admin;
+import com.example.coffeeproject.respoitory.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+    private final AdminRepository adminRepository;
+
+    @Autowired
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    public ResponseEntity<String> signup(Object admin){
+        if(isUserIdAvailable(admin)){
+            saveUser(admin);
+            return ResponseEntity.ok("회원가입 성공");
+        }else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 존재하는 ID");
+        }
+    }
+
+    public void saveUser(Object admin) {
+        adminRepository.save((Admin)admin);
+    }
+    private boolean isUserIdAvailable(Object admin) {
+        if (admin instanceof Admin) {
+            return !adminRepository.existsById(((Admin) admin).getAdmin_id());
+        } else {
+            throw new IllegalArgumentException("잘못된 사용자 유형");
+        }
+    }
+}
